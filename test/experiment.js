35c74f4de50a32e7aecedf14cf2d0942c31a6a87
elsehow <yes@cosmopol.is>
2016-05-04 21:37:40 -0700

    ..

--- test/experiment.js
+++ test/experiment.js
@@ -1,5 +1,6 @@
 'use strict'
 let obj = (k, p) => {let o = {}; o[k] = p; return o}
+let truthy = x => !!x
 let sameName = (place1, place2) => place1.name === place2.name
 let streamAndEmitF = require('../src/stream-and-emitF')
 let EventEmitter = require('events').EventEmitter
@@ -13,17 +14,14 @@ let spaces = spatial(kv)
 let readStream = spaces.createReadStream({ live: true })
 let [movementS, movementEmitF] = streamAndEmitF()
 let movementCb = (err, res) => {
-  //console.log('calling back on', err, res) // TODO debug
-  movementEmitF(res) 
+  console.log('MOVEMENT CB', err, res) // TODO debug
+  movementEmitF(res)
 }
-let moveTo = (place) => {
-  //console.log('moving to', place) // TODO debug
-  spaces.find(place, movementCb) 
-}
+let moveTo = (place) => spaces.find(place, movementCb)
 // spaceS is a stream of updates to places in the world.
 let spaceS = Kefir.fromEvents(readStream, 'data')
 // updates to places we're currently in.
-let updateToCurLocS = spaceS.combine(movementS, sameName)
+let updateToCurLocS = spaceS.combine(movementS.filter(truthy), sameName)
 //movementS.log('movement') // TODO debug
 // currentLocS is a stream of updates to the place we're currently in
 // they come either from having moved,
@@ -31,45 +29,33 @@ let updateToCurLocS = spaceS.combine(movementS, sameName)
 let currentLocS = spaceS.filterBy(updateToCurLocS).merge(movementS)
 let edgeToTextCommand = (e) => obj(e.command, () => moveTo(e.goesTo))
 let placeToTextCommands = (pl) => pl.edges.map(edgeToTextCommand)
-let currentMovementCommandS = currentLocS.map(placeToTextCommands)
+let commandsAt = (loc) => placeToTextCommands(loc).concat(globalCommands(spaces, loc))
 // produce currentCommandS - a stream of all the commands we can issue
 // these update along with currentLocS, since the commands available to us
-// are a function of where we are in the world.
-// at any given time.
-let currentCommandS = currentLocS
-    .map(l => placeToTextCommands(l).concat(globalCommands(spaces, l)))
+// are a function of where we are in the world, at any given time.
+// currentCommandS is a list of functions, each one can take some input and produce a side-effect
+let currentCommandS = currentLocS.map(commandsAt).map(require('text-commander'))
 
-function globalCommands (sp, curLoc) {
-  console.log('making commands for', curLoc) //TODO debug
-  // TODO need a way to error
-  let cb = (err, res) => {
-    if (err)
-      console.log('ERR!', err)
-  }
-  return [
-    //{ 'look' :  _ => sp.find(curLoc, movementCb) },
-    { 'describe: {description}': o => sp.describe(curLoc.name, o.description, cb) },
-    { "you can '{command}' to {place}": o => sp.link(curLoc.name, o.place, o.command, cb) },
-    { "you cannot '{command}'": o => sp.unlink(curLoc.name, o.command, cb) },
-    //{ '': cb },
-  ]
-}
+let inputS = Kefir.merge([
+  Kefir.later(1200, 'fall through the book'),
+  Kefir.later(1500, "you can 'jump into the water' to some water"),
+  Kefir.later(2000, "jump into the water"),
+])
+inputS.log('INPUT') // TODO debug
+Kefir.zip([inputS, currentCommandS]).onValue(([input, cmdr]) => cmdr(input))
 
-// TODO filter inputS through commands to produce side-effects
-let applyToInput = (f) => process.stdin.on('data', d => f(d.toString().trim()))
-currentCommandS
-  .map(require('text-commander')) // feed commands list into text-commander
-  .onValue(cmdr => applyToInput(cmdr)) // feed input into text commander
 
 // print updates to current location
-currentLocS
-  .map(descriptions.space)
-  .log()
+currentLocS.map(descriptions.space).log()
 
 
 
 
 /*
+
+TODO    what happens when a command isnt recognized?
+            (this could be a feature of text-commander...easily)
+
 TODO    what to do when the place doesn't exist............
 -----------------------------------------------------------
 we ought to see a falsey value on `spaces.find()`
@@ -77,6 +63,20 @@ i think that `description.space` could handle that
 the question is how to deal with the rest of the processing pipeline
 */
 
+function globalCommands (sp, curLoc) {
+  //console.log('making commands for', curLoc) //TODO debug
+  // TODO need a way to error
+  let cb = (err, res) => {}
+  //    if (err)
+  //      console.log('ERR!', err)
+  return [
+    //{ 'look' :  _ => sp.find(curLoc, movementCb) },
+    { 'describe: {description}': o => sp.describe(curLoc.name, o.description, cb) },
+    { "you can '{command}' to {place}": o => sp.link(curLoc.name, o.place, o.command, cb) },
+    { "you cannot '{command}'": o => sp.unlink(curLoc.name, o.command, cb) },
+    //{ '': cb }, // TODO need a way to catch commands we dont know about
+  ]
+}
 
 
 
